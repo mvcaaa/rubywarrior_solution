bd195002a47539f923ad98beb40ea53e49b983c5
Andrey Astashov <mvc.aaa@gmail.com>
2013-04-18 23:30:07 +0700

    lvl1

--- player.rb
+++ player.rb
@@ -1,19 +1,5 @@
 class Player
-
   def play_turn(warrior)
-    @health? @health = @health : @health = 0
-
-    if warrior.feel.empty? then 
-      if @health > warrior.health || warrior.health == 20 then
-        warrior.walk!
-      else
-        warrior.rest!
-      end
-    else 
-        warrior.attack! 
-    end
-
-      @health = warrior.health
+    warrior.walk!
   end
-
 end
